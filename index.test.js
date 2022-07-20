a82a885060303340361e1928d75e53e921b61345
Haneul Lee <hee1149kr@naver.com>
2022-07-20 19:01:59 +0900

    feat: Feat/tdd (#3)
    * chore: update readme.md
    
    * feat: 테스트 우선 개발
    
    * fix: update toc

--- index.test.js
+++ index.test.js
@@ -1,11 +1,51 @@
 const sut = require("./index");
+const { faker } = require("@faker-js/faker");
 
 test.each`
-  source              | expected
-  ${"hello  world"}   | ${"hello world"}
-  ${"hello   world"}  | ${"hello world"}
-  ${"hello    world"} | ${"hello world"}
+  source                  | expected
+  ${"hello  world"}       | ${"hello world"}
+  ${"hello   world"}      | ${"hello world"}
+  ${"hello    world"}     | ${"hello world"}
+  ${"hello     world"}    | ${"hello world"}
+  ${"hello      world"}   | ${"hello world"}
+  ${"hello       world"}  | ${"hello world"}
+  ${"hello        world"} | ${"hello world"}
 `("sut transtorms `$source` to `$expected`", ({ source, expected }) => {
   const actual = sut(source);
   expect(actual).toBe(expected);
+});
+
+test.each`
+  source             | expected
+  ${"hello\t world"} | ${"hello world"}
+  ${"hello \tworld"} | ${"hello world"}
+`(
+  'sut transtorms "$source" that contains tab character to "$expected"',
+  ({ source, expected }) => {
+    const actual = sut(source);
+    expect(actual).toBe(expected);
+  }
+);
+
+test.each`
+  source             | bannedWords              | expected
+  ${"hello mockist"} | ${["mockist", "purist"]} | ${"hello *******"}
+  ${"hello purist"}  | ${["mockist", "purist"]} | ${"hello ******"}
+`(
+  "sut transforms `$source` to `$expected`",
+  ({ source, bannedWords, expected }) => {
+    const actual = sut(source, { bannedWords });
+    expect(actual).toBe(expected);
+  }
+);
+
+describe("given banned word", () => {
+  const bannedWord = faker.lorem.word();
+  const source = "hello " + bannedWord;
+  const expected = "hello " + "*".repeat(bannedWord.length);
+
+  test("${bannedWord} then invoke sut then it returns ${expected}", () => {
+    const actual = sut(source, { bannedWords: [bannedWord] });
+    expect(actual).toBe(expected);
+  });
 });
