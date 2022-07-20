2bf16d2f405dca77be2dd2b497406a0709fad675
leesky <leehaneul0623@gmail.com>
2022-07-20 14:53:37 +0900

    feat: lifecycle hooks

--- jest/testcase/teardown/index.test.js
+++ jest/testcase/teardown/index.test.js
@@ -4,15 +4,30 @@
 // 많은 테스트를 위해 반복적으로 수행될 필요가 있는 작업이 있다면, beforeEach와 afterEach를 사용할 수 있습니다.
 
 const database = {};
+const foods = {};
 
 const isCity = (city) => {
   return database.cities.includes(city);
+};
+
+const isValidCityFoodPair = (city, food) => {
+  return foods[city] === food;
 };
 
 async function initializeCityDatabase() {
   return new Promise((resolve) => {
     setTimeout(() => {
       database.cities = ["Vienna", "San Juan"];
+      resolve();
+    }, 2000);
+  });
+}
+
+async function initializeFoodDatabase() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      foods.Vienna = "Wiener Schnitzel";
+      foods["San Juan"] = "Mofongo";
       resolve();
     }, 2000);
   });
@@ -60,3 +75,62 @@ test("city database has vienna", () => {
 test("city database has San Juan", () => {
   expect(isCity("San Juan")).toBeTruthy();
 });
+
+/*
+기본적으로, before와 after 블럭은 파일의 모든 테스트에 적용됩니다. describe 블럭을 사용하여 테스트들을 그룹핑할 수도 있습니다. 
+describe 블럭 안에 있을 경우, before와 after 블럭들은 그 describe 블럭 내부의 테스트에만 적용됩니다.
+*/
+
+// 모든 테스트 적용
+beforeEach(() => {
+  return initializeCityDatabase();
+});
+
+test("city database has Vienna", () => {
+  expect(isCity("Vienna")).toBeTruthy();
+});
+
+test("city database has San Juan", () => {
+  expect(isCity("San Juan")).toBeTruthy();
+});
+
+describe("matching cities to foods", () => {
+  beforeEach(() => {
+    return initializeFoodDatabase();
+  });
+
+  test("Vienna <3 sausage", () => {
+    expect(isValidCityFoodPair("Vienna", "Wiener Schnitzel")).toBe(true);
+  });
+
+  test("San Juan <3 plantains", () => {
+    expect(isValidCityFoodPair("San Juan", "Mofongo")).toBe(true);
+  });
+});
+
+//
+//
+// beforeAll(() => console.log('1 - beforeAll'));
+// afterAll(() => console.log('1 - afterAll'));
+// beforeEach(() => console.log('1 - beforeEach'));
+// afterEach(() => console.log('1 - afterEach'));
+// test('', () => console.log('1 - test'));
+// describe('Scoped / Nested block', () => {
+//   beforeAll(() => console.log('2 - beforeAll'));
+//   afterAll(() => console.log('2 - afterAll'));
+//   beforeEach(() => console.log('2 - beforeEach'));
+//   afterEach(() => console.log('2 - afterEach'));
+//   test('', () => console.log('2 - test'));
+// });
+// 1 - beforeAll
+// 1 - beforeEach
+// 1 - test
+// 1 - afterEach
+// 2 - beforeAll
+// 1 - beforeEach
+// 2 - beforeEach
+// 2 - test
+// 2 - afterEach
+// 1 - afterEach
+// 2 - afterAll
+// 1 - afterAll
