5d6cf6d9321e304bbb3f5e0f561072496b47a85a
Haneul Lee <hee1149kr@naver.com>
2022-07-21 22:26:40 +0900

    feat: tdd introduction (#5)

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-function refineText(s, options) {
-  s = s
-    .replace("    ", " ")
-    .replace("\t", " ")
-    .replace("   ", " ")
-    .replace("   ", " ")
-    .replace("  ", " ")
-    .replace("mockist", "*******")
-    .replace("purist", "******");
+function refineText(source, options) {
+  return [
+    normalizeWhitespaces,
+    compactWhitespaces,
+    maskBannedWords,
+    trimWhitespaces,
+  ].reduce((value, filter) => filter(value, options), source);
+}
 
-  if (options) {
-    for (const bannedWord of options.bannedWords) {
-      s = s.replace(bannedWord, "*".repeat(bannedWord.length));
-    }
-  }
+function normalizeWhitespaces(value) {
+  return value.replace("\t", " ");
+}
 
-  return s;
+function compactWhitespaces(value) {
+  return value.indexOf("  ") < 0
+    ? value
+    : compactWhitespaces(value.replace("  ", " "));
+}
+
+function maskBannedWords(value, options) {
+  return options ? options.bannedWords.reduce(maskBannedWord, value) : value;
+}
+
+function maskBannedWord(value, bannedWord) {
+  return value.replace(bannedWord, "*".repeat(bannedWord.length));
+}
+
+function trimWhitespaces(value) {
+  return value.trim();
 }
 
 module.exports = refineText;
